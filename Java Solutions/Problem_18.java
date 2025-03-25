eeb9c858343132c3f982203829ad41127b8e940a
Rohit Gurav <108394069+RohitGurav1012@users.noreply.github.com>
2025-03-25 20:31:06 +0530

    Update Problem_18.java

--- Java Solutions/Problem_18.java
+++ Java Solutions/Problem_18.java
@@ -1,29 +1,33 @@
 public class Problem_18 {
     public static void main(String[] args) {
-        int num = 5;  
-        char letter = 'D';  
-
-        while (num > 0) {
-            //This is for Print numbers
-            int tempNum = num;
-            while (tempNum > 0) {
-                System.out.print(tempNum + " ");
-                tempNum--;
-            }
-            System.out.println();
-
-            // this is for Print letters only if num > 1
-            if (num > 1) {
-                char tempChar = letter;
-                while (tempChar >= 'A') {
-                    System.out.print(tempChar + " ");
-                    tempChar--;
-                }
-                System.out.println();
-            }
-
-            num -= 2;  
-            letter -= 2;  
+        int[] numberRows = {5, 4, 3, 2, 1}; // Numbers pattern
+        char[] charRows = {'D', 'C', 'B', 'A'}; // Characters pattern
+        
+        // First Row (Numbers)
+        for (int i = 5; i >= 1; i--) {
+            System.out.print(i + " ");
         }
+        System.out.println();
+
+        // Second Row (Characters)
+        for (char ch = 'D'; ch >= 'A'; ch--) {
+            System.out.print(ch + " ");
+        }
+        System.out.println();
+
+        // Third Row (Numbers)
+        for (int i = 3; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Fourth Row (Characters)
+        for (char ch = 'B'; ch >= 'A'; ch--) {
+            System.out.print(ch + " ");
+        }
+        System.out.println();
+
+        // Fifth Row (Single Number)
+        System.out.println("1");
     }
 }
